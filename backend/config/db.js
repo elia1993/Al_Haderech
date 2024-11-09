83992f95a418c86050e7cc65f8bba80852ebf161
shreyJS <deshpandeshreyas850@gmail.com>
2024-11-10 03:11:30 +0530

    change in config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://deshpandeshreyas850:E83PYvK3KyJQVl2o@cluster0.3gvst.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB Connected"));
    
 }
 
