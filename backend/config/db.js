5e9bddfd8bfe4c524cf836580fdc852f0fa7bdd2
elia1993 <77536857+elia1993@users.noreply.github.com>
2024-11-22 21:24:09 +0200

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            ssl: true,             // Enable SSL
-            sslValidate: false,    // Disable SSL validation (for non-production environments)
-        });
-        console.log("DB Connected");
-    } catch (err) {
-        console.error("Error connecting to DB: ", err);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true, 
+    });
+    console.log("DB Connected");
+  } catch (err) {
+    console.error("Error connecting to DB:", err);
+  }
 };
