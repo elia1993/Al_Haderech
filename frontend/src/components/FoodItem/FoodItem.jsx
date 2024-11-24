ab2abd4186963eea94f0e4cd4a7ae43d0799734e
elia1993 <elia.mjdlany93@gmail.com>
2024-11-24 14:33:52 +0200

    Some changes..

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -12,7 +12,7 @@ const FoodItem = ({ image, name, price, desc , id }) => {
         <div className='food-item'>
             <div className='food-item-img-container'>
                 <img className='food-item-image' src={url+"/images/"+image} alt="" />
-                {!cartItems[id]
+                {!cartItems?.[id]
                 ?<img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
                 :<div className="food-item-counter">
                         <img src={assets.remove_icon_red} onClick={()=>removeFromCart(id)} alt="" />
