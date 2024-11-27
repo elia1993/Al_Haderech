6367548a9346f9c838ad1e49a6f4cdeacd80a01f
elia1993 <elia.mjdlany93@gmail.com>
2024-11-27 14:34:05 +0200

    Updated header icons to #19beff color

--- backend/routes/bentoRouter.js
+++ backend/routes/bentoRouter.js
@@ -7,7 +7,5 @@ const bentoRouter = express.Router();
 bentoRouter.post('/', createBento); // Create Bento
 bentoRouter.get('/', getAllBentos); // Get all Bentos for a user
 bentoRouter.delete('/:itemId', removeBento); // Remove a Bento using itemId in the URL
-router.get('/', (req, res) => {
-    res.json({ message: 'List of Bento items' });
-  });
+
 export default bentoRouter;
