25e1d23cf64e5276fd620d1d27b2c417fb5e2ed8
elia1993 <elia.mjdlany93@gmail.com>
2024-12-03 14:57:06 +0200

    here

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { 
-    port: 5174, 
-  },
-  define: {
-    'process.env': {
-      VITE_API_URL: process.env.VITE_API_URL || 'http://localhost:4000/api',
-      VITE_ADMIN_API_URL: process.env.VITE_ADMIN_API_URL || 'http://localhost:4000/api',
-    },
-  },
-});
+  server: { port: 5174 },
+})
