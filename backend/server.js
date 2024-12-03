83f94e56564e7152df30dd875b6bd8fb5c58f54e
elia1993 <elia.mjdlany93@gmail.com>
2024-12-03 15:01:38 +0200

    here

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use("/images", express.static('uploads'));
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 app.use("/api/bentos", bentoRouter);
-
+app.use("/api/food/list",foodRouter),
 console.log("API Routes Registered:");
 console.log("/api/user");
 console.log("/api/food");
